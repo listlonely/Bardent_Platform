fc76cd3124a643fcfb460791090c75d9b73b5298
listlonely <93426692+listlonely@users.noreply.github.com>
2023-04-22 21:02:20 +0800

    Add files via upload

---
+++ Platformer-Tutorial-part25/Assets/Scripts/Player/Data/PlayerData.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="newPlayerData", menuName ="Data/Player Data/Base Data")]
+public class PlayerData : ScriptableObject
+{
+    [Header("Move State")]
+    public float movementVelocity = 10f;
+
+    [Header("Jump State")]
+    public float jumpVelocity = 15f;
+    public int amountOfJumps = 1;
+
+    [Header("Wall Jump State")]
+    public float wallJumpVelocity = 20;
+    public float wallJumpTime = 0.4f;
+    public Vector2 wallJumpAngle = new Vector2(1, 2);
+
+    [Header("In Air State")]
+    public float coyoteTime = 0.2f;
+    public float variableJumpHeightMultiplier = 0.5f;
+
+    [Header("Wall Slide State")]
+    public float wallSlideVelocity = 3f;
+
+    [Header("Wall Climb State")]
+    public float wallClimbVelocity = 3f;
+
+    [Header("Ledge Climb State")]
+    public Vector2 startOffset;
+    public Vector2 stopOffset;
+
+    [Header("Check Variables")]
+    public float groundCheckRadius = 0.3f;
+    public float wallCheckDistance = 0.5f;
+    public LayerMask whatIsGround;
+}
